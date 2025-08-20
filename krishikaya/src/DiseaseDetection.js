4843a9d70dfebb5a50a9bd809ae557d4e278f69a
Gaurav Agarwal <145758831+gaurav312agarwal@users.noreply.github.com>
2025-08-20 15:39:55 +0530

    Update DiseaseDetection.js

--- krishikaya/src/DiseaseDetection.js
+++ krishikaya/src/DiseaseDetection.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
+const API_URL = process.env.REACT_APP_API_URL || 'https://krishikaya-one-stop-farming.onrender.com';
 
 function DiseaseDetection() {
   const [selectedFile, setSelectedFile] = useState(null);
